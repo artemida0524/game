44247ac971e988f205bb756a3c7f12655a9ef6c9
artemida0524 <artemovcharyk1982@gmail.com>
2024-03-07 11:10:12 +0200

    7

--- Game/Assets/Scripts/Scripts/WaterPhysicsForPlayer.cs
+++ Game/Assets/Scripts/Scripts/WaterPhysicsForPlayer.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class WaterPhysicsForPlayer : MonoBehaviour
 {
-    
+
     public float floatUpSpeedLimit = 1.15f;
     public float floatUpSpeed = 4f;
 
     private void Start()
     {
-        
+
     }
     private void OnTriggerStay(Collider other)
     {
-        
-            if (other.gameObject.layer == 4)
-            {
 
-                float difference = (other.transform.position.y - transform.position.y) * floatUpSpeed;
+        if (other.gameObject.layer == 4)
+        {
+
+            float difference = (other.transform.position.y - transform.position.y) * floatUpSpeed;
 
             GetComponent<Rigidbody>().AddForce(new Vector3(0f, Mathf.Clamp((Mathf.Abs(Physics.gravity.y) * difference), 0, Mathf.Abs(Physics.gravity.y) * floatUpSpeedLimit), 0f), ForceMode.Acceleration);
             GetComponent<Rigidbody>().drag = 0.99f;
             GetComponent<Rigidbody>().angularDrag = 0.8f;
-            }
-        
-        
+        }
+
+
 
     }
     private void OnTriggerExit(Collider other)
