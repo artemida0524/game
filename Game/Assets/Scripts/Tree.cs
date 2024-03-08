0e0feed3c9ac0ed5466a8b745a803fb24acc1551
artemida0524 <artemovcharyk1982@gmail.com>
2024-03-08 16:50:27 +0200

    12

---
+++ Game/Assets/Scripts/Tree.cs
@@ -0,0 +1,31 @@
+using Cinemachine.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Tree : MonoBehaviour
+{
+
+    [SerializeField] private float time = 0.5f;
+    private float timeUp = 0;
+    [SerializeField] GameObject obj;
+    private void Update()
+    {
+        timeUp += Time.deltaTime;
+        if (timeUp >= time)
+        {
+            Vector3 vector3 = transform.position;
+            vector3.x += Random.Range(-4.0f, 4.0f);
+            vector3.y += Random.Range(3f, 6.0f);
+            vector3.z += Random.Range(-4.0f, 4.0f);
+            timeUp = 0;
+            GameObject app =  Instantiate(obj, vector3, Quaternion.identity);
+
+            
+
+        }
+    }
+}
